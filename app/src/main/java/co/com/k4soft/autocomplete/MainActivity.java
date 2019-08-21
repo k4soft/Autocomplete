db87069d08dc3e686b2f759790670ce865ad2666
diegogarcia <consultor@dgpragmatic.com>
2019-08-21 18:27:58 -0500

    Se adiciona Spinner

--- app/src/main/java/co/com/k4soft/autocomplete/MainActivity.java
+++ app/src/main/java/co/com/k4soft/autocomplete/MainActivity.java
@@ -6,23 +6,52 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private AutoCompleteTextView autoComplete;
+    private Spinner spinnerDiasSemana;
     private String[] lenguages = {"C", "C#", "Java","Python","PHP","Swift"};
+
+    private String[] dias = {"Lunes", "Martes", "Miercoles", "Jueves", "Viernes","Sábado","Domingo"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initComponents();
-        loadInfo();
-        seletedListener();
+        loadInfoAutomComplete();
+        loadInfoSpinner();
+        seletedAutocompleteListener();
+        selectedSpinnerItem();
     }
 
-    private void seletedListener() {
+    private void selectedSpinnerItem() {
+        spinnerDiasSemana.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(),getDia(i),Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+
+    private String getDia(int i) {
+        return dias[i];
+    }
+
+    private void loadInfoSpinner() {
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,R.layout.support_simple_spinner_dropdown_item,dias);
+        spinnerDiasSemana.setAdapter(arrayAdapter);
+    }
+
+    private void seletedAutocompleteListener() {
         autoComplete.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
@@ -35,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
         return lenguages[i];
     }
 
-    private void loadInfo() {
+    private void loadInfoAutomComplete() {
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,android.R.layout.select_dialog_item,lenguages);
         autoComplete.setThreshold(1);
         autoComplete.setAdapter(arrayAdapter);
@@ -43,5 +72,6 @@ public class MainActivity extends AppCompatActivity {
 
     private void initComponents() {
         autoComplete = findViewById(R.id.autoComplete);
+        spinnerDiasSemana = findViewById(R.id.spinnerDiasSemana);
     }
 }
